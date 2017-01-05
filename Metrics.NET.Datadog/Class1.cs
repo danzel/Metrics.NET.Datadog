0905f45cf5ea72c41e44566d7e91588d810c2b7c
danzel <dave@smartrak.co.nz>
2017-01-06 10:13:51 +1300

    Add project files.

---
+++ Metrics.NET.Datadog/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metrics.NET.Datadog
+{
+    public class Class1
+    {
+    }
+}
